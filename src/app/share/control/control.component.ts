8035d84b1ae831993c5d3830bda637d3a37d38a7
Veng-Eang <oengvengeang@gmail.com>
2025-01-13 19:57:05 +0700

    host

--- src/app/share/control/control.component.ts
+++ src/app/share/control/control.component.ts
@@ -1,11 +1,24 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation ,HostBinding, HostListener, inject, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class : 'control',
+    '(click)' : 'onClick()'
+  }
 })
 export class ControlComponent {
+// @HostBinding('class') className = 'control';
+// @HostBinding('id') idName = 'Titlee';
 label=input.required<string>();
+private el = inject(ElementRef);
+// @HostListener('click')
+onClick(){
+  console.log("Clicked!");
+  console.log(this.el);
+}
 }
